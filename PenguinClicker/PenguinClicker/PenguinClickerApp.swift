37253b3712f7aa41624c234cf44b8657a4063f20
Michael Mathews <michael.mathews1999@gmail.com>
2023-11-14 23:29:39 -0800

    penguin clicker project creation

---
+++ PenguinClicker/PenguinClicker/PenguinClickerApp.swift
@@ -0,0 +1,20 @@
+//
+//  PenguinClickerApp.swift
+//  PenguinClicker
+//
+//  Created by Michael Mathews on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct PenguinClickerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
